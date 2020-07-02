d2d35aaca6c3471e0c13324a3fa375444cb61aa7
Yinghao Wang <michaelwang0905@icloud.com>
2020-07-01 21:22:24 -0700

    Initial commit of navigation for task 5, 9, 13.

---
+++ walker_WAIC_18.04_v1.2_20200616/ubt_sim_ws/src/thewalkingdead/scripts/navigation.py
@@ -0,0 +1,246 @@
+import rospy
+import tf
+import dynamic_reconfigure.client
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Int64, String
+
+import functools
+import threading
+import math
+
+PI = 3.141592654
+
+def _norm(x, y):
+    return functools.reduce(lambda a, b : a+b, map(lambda (a, b) : (a-b)*(a-b), zip(x, y)))
+
+def _todeg(rad):
+    return rad * 57.2957795
+
+def _torad(deg):
+    return deg / 180.0 * PI
+
+def _origin_symmetric_angle(rad):
+    p = [-math.cos(rad), -math.sin(rad)]
+    return math.atan2(p[1], p[0])
+
+class Navigation(object):
+    def __init__(self):
+        # self.dcfg_client = dynamic_reconfigure.client.Client("orb_slam2_stereo", 
+        #     timeout=10, config_callback=self._configure_orb_slam_callback)
+        
+        self._lock = threading.Lock()
+        self._lock_step = threading.Lock()
+        self._lock_stat = threading.Lock()
+
+        self.position = None
+        self.yaw = None
+        self.total_step = 0
+
+        self.odom_listner = rospy.Subscriber("/odom", Odometry, self._odom_callback, queue_size=10)
+        self.step_listener = rospy.Subscriber("/Leg/StepNum", Int64, self._step_callback, queue_size=10)
+        self.nav_pub = rospy.Publisher("/nav/cmd_vel_nav", Twist, queue_size=10, latch=True)
+
+    def _configure_orb_slam_callback(self, config):
+        rospy.loginfo("Navigation::Current orbslam2 config: %s", str(config))
+
+    def _odom_callback(self, d):
+        with self._lock:
+            self.position = [d.pose.pose.position.x, d.pose.pose.position.y]
+            self._rot = d.pose.pose.orientation
+            self._rot_e = tf.transformations.euler_from_quaternion([self._rot.x, 
+                                                                    self._rot.y,
+                                                                    self._rot.z,
+                                                                    self._rot.w])
+            self.yaw = self._rot_e[2]
+    
+    def _step_callback(self, d):
+        with self._lock_step:
+            self.total_step = int(d.data)
+
+    def _wait_for_odom_data(self):
+        while self.position is None or self.yaw is None:
+            pass
+
+    def _nav_msg(self, lx=0.0, ly=0.0, lz=0.0, ax=0.0, ay=0.0, az=0.0):
+        t = Twist()
+        t.linear.x = lx
+        t.linear.y = ly
+        t.linear.z = lz
+        t.angular.x = ax
+        t.angular.y = ay
+        t.angular.z = az
+        return t
+
+    def _compute_steps_angle(self, step_lengths, angle):
+        step_list = []
+        for l in step_lengths:
+            s = math.floor(angle / l)
+            angle = angle - s * l
+            step_list.append(s * 2)  # When turning, 2 steps make one turn
+        return step_list
+
+    def _compute_steps_linear(self, step_lengths, x):
+        step_list = []
+        for l in step_lengths:
+            s = math.floor(x / l)
+            x = x - s * l
+            step_list.append(s)
+        return step_list
+
+    def _compute_target_yaw_dist(self, cur_pos, tar_pos):
+        d = [b-a for a, b in zip(cur_pos, tar_pos)]
+        norm = math.sqrt(functools.reduce(lambda a,b : a+b, [x*x for x in d]))
+        d = [x / norm for x in d]
+        yaw = math.acos(d[0]) if d[1] > 0 else -math.acos(d[0])
+        return yaw, norm
+
+    # To Test
+    # def _compute_target_yaw_dist(self, cur_pos, cur_yaw, tar_pos, tar_yaw):
+    #     d = [b-a for a, b in zip(cur_pos, tar_pos)]
+    #     norm = math.sqrt(functools.reduce(lambda a,b : a+b, [x*x for x in d]))
+    #     d = [x / norm for x in d]
+    #     yaw = math.acos(d[0]) if d[1] > 0 else -math.acos(d[0])
+
+    #     e = [math.cos(cur_yaw), math.sin(cur_yaw)]
+    #     a = math.acos(d, e)
+    #     if a < PI / 2:
+    #         tyaw = yaw
+    #         norm = norm
+    #     else:
+    #         tyaw = _origin_symmetric_angle(yaw)
+    #         norm = -norm
+
+    #     return yaw, norm
+
+
+    def _wait_for_step(self):
+        with self._lock_step:
+            cur_step = start_step = self.total_step
+        while start_step == cur_step:
+            with self._lock_step:
+                cur_step = self.total_step
+
+    def halt(self, t=1.0):
+        r = rospy.Rate(10)
+        s = 0.0
+        while s < t:
+            self.nav_pub.publish(self._nav_msg())
+            s += 0.1
+            r.sleep()
+
+    def turn_to(self, t_yaw):
+        with self._lock:
+            yaw = self.yaw
+        
+        self._wait_for_step()
+        
+        angle = (t_yaw - yaw + 2 * PI) % (2*PI)
+        sign = 1 # ccw
+        if angle > PI:
+            angle = 2*PI - angle
+            sign = -1 # cw 
+
+        step_lengths = [0.35, 0.175, 0.0875, 0.04375, 0.021875, 0.0109375, 0.00546875]
+        steps = self._compute_steps_angle(step_lengths, angle)
+        rospy.loginfo("Angle: %f, direction: %s, Steps to turn: %s", angle, 
+            'CCW' if sign > 0 else 'CW', str(steps))
+        
+        r = rospy.Rate(10)
+        with self._lock_step:
+            cur_step = self.total_step
+        for i in range(len(steps)):
+            tar_step = cur_step + steps[i]
+            while cur_step < tar_step:
+                self.nav_pub.publish(self._nav_msg(az=step_lengths[i]*sign))
+                with self._lock_step:
+                    cur_step = self.total_step
+                r.sleep()
+
+    def move_for(self, t_x):
+        self._wait_for_step()
+
+        step_lengths = [0.32, 0.16, 0.08, 0.04, 0.02, 0.01, 0.005]
+        step_list = self._compute_steps_linear(step_lengths, abs(t_x))
+        sign = 1 if t_x > 0 else -1
+        rospy.loginfo("t_x: %f, Steps to walk: %s, Direction: %s",
+                        t_x, str(step_list), 'forward' if sign > 0 else 'backward')
+
+        r = rospy.Rate(10)
+        with self._lock_step:
+            step = cur_step = self.total_step
+        for i in range(len(step_list)):
+            tar_step = cur_step + step_list[i]
+            while cur_step < tar_step:
+                self.nav_pub.publish(self._nav_msg(lx=step_lengths[i]*sign))
+                with self._lock_step:
+                    cur_step = self.total_step
+                r.sleep()
+
+    def move_sideway_for(self, t):
+        self._wait_for_step()
+
+        step_lengths = [0.04]
+        step_list = self._compute_steps_linear(step_lengths, abs(t))
+        sign = 1 if t > 0 else -1
+        rospy.loginfo("t: %f, Steps to walk: %s, Direction: %s",
+                        t, str(step_list), 'left' if sign > 0 else 'right')
+
+        r = rospy.Rate(10)
+        with self._lock_step:
+            step = cur_step = self.total_step
+        for i in range(len(step_list)):
+            tar_step = cur_step + step_list[i]
+            while cur_step < tar_step:
+                self.nav_pub.publish(self._nav_msg(ly=step_lengths[i]*sign))
+                with self._lock_step:
+                    cur_step = self.total_step
+                r.sleep()
+
+    def correct_x(self, tar_x):
+        self._wait_for_odom_data()
+        with self._lock:
+            cur_x, _ = self.position
+        d = cur_x - tar_x
+        self.move_sideway_for(d)
+
+    def go_to(self, tar_pos):
+        self._wait_for_odom_data()
+        with self._lock:
+            cur_pos = self.position
+        rospy.logdebug("Current position: %s, Target position: %s", str(cur_pos), str(tar_pos))
+        tar_yaw, tar_dist = self._compute_target_yaw_dist(cur_pos, tar_pos)
+        rospy.logdebug("Target yaw: %s, target distance: %s", str(tar_yaw), str(tar_dist))
+
+        rospy.logdebug("GOTO::Starting turn_to...")
+        self.turn_to(tar_yaw)
+        rospy.logdebug("GOTO::Starting move for...")
+        self.move_for(tar_dist)
+        # Test this in the morning
+
+    def wait_for_orb_slam(self):
+        rospy.wait_for_message("/orb_slam2_stereo/map_points", PointCloud2, None)
+    
+    def configure_orb_slam_localization(self):
+        self.dcfg_client.update_configuration({"localize_only": True})
+
+    def relocalize(self):
+        cur_position = last_position = [0.0, 0.0]
+        dist = 0.0
+
+        r = rospy.Rate(10)
+        while dist < 5.0:
+            last_position = cur_position
+            with self._lock:
+                cur_position = self.position if self.position is not None else cur_position
+            dist = _norm(last_position, cur_position)
+            try:
+                self.nav_pub.publish(self._nav_msg(az=-0.35))
+            except rospy.ServiceException as e:
+                rospy.logerror("Navigation::Failure in sending message to /nav/cmd_vel_nav")
+                # raise e # try keep sending.
+            
+            r.sleep()
+        
+        rospy.loginfo("Navigation::Detected large position shift, assuming relocalization happened...")
---
+++ walker_WAIC_18.04_v1.2_20200616/ubt_sim_ws/src/thewalkingdead/src/odom_publisher.cpp
@@ -0,0 +1,266 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/Imu.h>
+
+#include <iostream>
+#include <mutex>
+
+double camlink_x;
+double camlink_y;
+double camlink_z;
+
+double odom_x;
+double odom_y;
+double odom_z;
+
+double orb_x;
+double orb_y;
+double orb_z;
+double orb_qx;
+double orb_qy;
+double orb_qz;
+double orb_qw;
+
+double imu_r;
+double imu_p;
+double imu_y;
+
+double imu_vr;
+double imu_vp;
+double imu_vy;
+
+double imu_ax;
+double imu_ay;
+double imu_az;
+
+nav_msgs::Odometry walking_odom;
+
+ros::Time last_tick_main, last_tick_imu;
+double vx;
+double vy;
+double vz;
+
+double dvx;
+double dvy;
+double dvz;
+
+void orb_pose_callback(const geometry_msgs::PoseStamped &poseStamped)
+{
+    double last_orb_x = orb_x;
+    double last_orb_y = orb_y;
+    double last_orb_z = orb_z;
+
+    orb_x = poseStamped.pose.position.x;
+    orb_y = poseStamped.pose.position.y;
+    orb_z = poseStamped.pose.position.z;
+
+    orb_qx = poseStamped.pose.orientation.x;
+    orb_qy = poseStamped.pose.orientation.y;
+    orb_qz = poseStamped.pose.orientation.z;
+    orb_qw = poseStamped.pose.orientation.w;
+
+    // ros::Duration dt = ros::Time::now() - last_tick;
+    // if (dt.toSec() < 1e-5)
+    //     return;
+    
+    // double dx = orb_x - last_orb_x;
+    // double dy = orb_y - last_orb_y;
+
+    // std::cout << ros::Time::now() << " " << last_tick << std::endl;
+    // std::cout << dx << " " << dy << " " << dt << std::endl;
+    // vx = dx / dt.toSec();
+    // vy = dy / dt.toSec();
+
+    // last_tick = last_tick + dt;
+}
+
+
+
+void imu_callback(const sensor_msgs::Imu &imuData)
+{
+    imu_r = imuData.orientation.x;
+    imu_p = imuData.orientation.y;
+    imu_y = imuData.orientation.z;
+
+    imu_vr = imuData.angular_velocity.x;
+    imu_vp = imuData.angular_velocity.y;
+    imu_vy = imuData.angular_velocity.z;
+
+    imu_ax = imuData.linear_acceleration.x;
+    imu_ay = imuData.linear_acceleration.y;
+    imu_az = imuData.linear_acceleration.z;
+
+    // double dt = (ros::Time::now() - last_tick_imu).getSec();
+    // dvx += imu_ax * dt;
+    // dvy += imu_ay * dt;
+
+    // vx += dvx;
+    // vy += dvy;
+}
+
+void walking_odom_callback(const nav_msgs::Odometry &w_odom_data)
+{
+    walking_odom = w_odom_data;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "odometry_publisher");
+
+    ros::NodeHandle n;
+    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50, true);
+    tf::TransformBroadcaster odom_broadcaster;
+    tf::TransformListener listener;
+
+    vx = 0.0;
+    vy = 0.0;
+    vz = 0.0;
+
+    dvx = 0.0;
+    dvy = 0.0;
+    dvz = 0.0;
+
+    orb_x = 0.0;
+    orb_y = 0.0;
+    orb_z = 0.0;
+    orb_qx = 0.0;
+    orb_qy = 0.0;
+    orb_qz = 0.0;
+    orb_qw = 1.0;
+
+    camlink_x = 0.0;
+    camlink_y = 0.0;
+    camlink_z = 0.0;
+
+    imu_r = 0.0;
+    imu_p = 0.0;
+    imu_y = 0.0;
+
+    imu_vr = 0.0;
+    imu_vp = 0.0;
+    imu_vy = 0.0;
+
+    imu_ax = 0.0;
+    imu_ay = 0.0;
+    imu_az = 0.0;
+
+    walking_odom = nav_msgs::Odometry();
+
+    last_tick_main = ros::Time::now();
+    last_tick_imu = ros::Time::now();
+
+    ros::Subscriber orb_sub = n.subscribe("/orb_slam2_stereo/pose", 10, orb_pose_callback);
+    // ros::Subscriber imu_sub = n.subscribe("/sensor/camera_imu", 10, imu_callback);
+    // ros::Subscriber wodom_sub = n.subscribe("/Leg/walking_odom", 10, walking_odom_callback);
+
+    // tf::StampedTransform last_camlink_transform;
+    // last_camlink_transform.setData(tf::Transform::getIdentity());
+
+    ros::Rate r(1000);
+    ros::Time cur;
+    while (n.ok())
+    {
+        cur = ros::Time::now();
+        ros::spinOnce(); // check for incoming messages
+
+        //For rotation, we will use IMU's orientation, since it's higher frequency.
+        // tf::Matrix3x3 mat;
+        // mat.setRPY(imu_r, imu_p, imu_y - 3.141592654 * 0.5);
+        // tf::Quaternion odom_quat;
+        // mat.getRotation(odom_quat);
+        tf::StampedTransform map_camlink_transform;
+        tf::StampedTransform map_odom_transform;
+        try {
+            listener.waitForTransform("/map", "/stereo_camera_link", ros::Time(0), ros::Duration(10.0));
+            listener.lookupTransform("/map", "/stereo_camera_link", ros::Time(0), map_camlink_transform);
+        }
+        catch (tf::TransformException ex) {
+            ROS_ERROR("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+
+        // try {
+        //     listener.waitForTransform("/map", "/odom", ros::Time(0), ros::Duration(10.0));
+        //     listener.lookupTransform("/map", "/odom", ros::Time(0), map_odom_transform);
+        // }
+        // catch (tf::TransformException ex) {
+        //     ROS_ERROR("%s", ex.what());
+        //     ros::Duration(1.0).sleep();
+        //     continue;
+        // }
+
+        camlink_x = map_camlink_transform.getOrigin().getX();
+        camlink_y = map_camlink_transform.getOrigin().getY();
+        camlink_z = map_camlink_transform.getOrigin().getZ();
+
+        // odom_x = map_odom_transform.getOrigin().getX();
+        // odom_y = map_odom_transform.getOrigin().getY();
+        // odom_z = map_odom_transform.getOrigin().getZ();
+
+        //first, we'll publish the transform over tf
+        geometry_msgs::TransformStamped odom_trans;
+        odom_trans.header.stamp = cur;
+        odom_trans.header.frame_id = "odom";
+        odom_trans.child_frame_id = "base_link";
+
+        odom_trans.transform.translation.x = camlink_x;
+        odom_trans.transform.translation.y = camlink_y;
+        odom_trans.transform.translation.z = 0.0;
+        quaternionTFToMsg(map_camlink_transform.getRotation(), odom_trans.transform.rotation);
+
+        //send the transform
+        odom_broadcaster.sendTransform(odom_trans);
+
+        // Provide a map/odom frame for cartographer, force to be static
+        // geometry_msgs::TransformStamped map_trans; 
+        // map_trans.header.stamp = cur;
+        // map_trans.header.frame_id = "map";
+        // map_trans.child_frame_id = "odom";
+        // map_trans.transform.translation.x = 0.0;
+        // map_trans.transform.translation.y = 0.0;
+        // map_trans.transform.translation.z = 0.0;
+        // quaternionTFToMsg(tf::Quaternion(0, 0, 0, 1), map_trans.transform.rotation);
+
+        // odom_broadcaster.sendTransform(map_trans);
+
+        //next, we'll publish the odometry message over ROS
+        nav_msgs::Odometry odom;
+        odom.header.stamp = cur;
+        odom.header.frame_id = "odom";
+
+        //set the position, using data from visual odom
+        odom.pose.pose.position.x = camlink_x;
+        odom.pose.pose.position.y = camlink_y;
+        odom.pose.pose.position.z = 0.0;
+        quaternionTFToMsg(map_camlink_transform.getRotation(), odom.pose.pose.orientation);
+
+        // double dt = (cur - last_tick_main).toSec();
+        // if (dt > 1e-5)
+        // {
+        //     vx = (camlink_x - last_camlink_transform.getOrigin().getX()) / dt;
+        //     vy = (camlink_y - last_camlink_transform.getOrigin().getY()) / dt;
+        // }
+        // last_tick_main = cur;
+        // last_camlink_transform = map_camlink_transform;
+
+        // //set the velocity, using data from IMU integral angular_v readout
+        // odom.child_frame_id = "base_link";
+        // odom.twist.twist.linear.x = vx;
+        // odom.twist.twist.linear.y = vy;
+        // odom.twist.twist.angular.z = imu_vy;
+
+        odom.child_frame_id = "base_link";
+        odom.twist.twist.linear.x = 0.0;
+        odom.twist.twist.linear.y = 0.0;
+        odom.twist.twist.angular.z = 0.0;
+
+        //publish the message
+        odom_pub.publish(odom);
+        
+        r.sleep();
+    }
+}
